8477b348e28ad1974a847e05461aba82b528e35d
omurthy <omuralcinthy@gmail.com>
2023-04-04 16:54:05 +0300

    adds cors policy allow

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const dbConfig = require("./app/config/db.config");
 const app = express();
 
 var corsOptions = {
-  origin: ["http://localhost:8081"],
+  origin: ["https://authopher-server.vercel.app/"],
   credentials: true
 }
 
